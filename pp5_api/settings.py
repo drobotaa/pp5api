5978775e195a664cba4b05aa0c196d437d967a0a
Andrei Drobota <z68sbi67@students.codeinstitute.net>
2024-07-17 16:28:04 +0000

    Added allowed_host and gitpod client origin dev

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import re
 
 if os.path.exists('env.py'):
     import env
@@ -63,8 +64,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
+    os.environ.get('ALLOWED_HOST'),
     'localhost',
-    'pp5-drobota-bd94aa0a3e49.herokuapp.com',
     '8000-drobotaa-pp5api-gze2zixq19w.ws-eu115.gitpod.io'
 ]
 
@@ -118,9 +119,10 @@ if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN')
     ]
-else:
-    CORS_ALLOWED_ORIGINS_REGEXES = [
-        r"^https://.*\.gitpod\.io$",
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
 CORS_ALLOW_CREDENTIALS = True
