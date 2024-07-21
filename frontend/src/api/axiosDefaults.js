f9057e8267ca95133028f199210de027febda2ce
Andrei Drobota <z68sbi67@students.codeinstitute.net>
2024-07-21 08:48:07 +0000

    Removed baseURL from axiosDefault, and added proxy to packagejson

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = 'https://pp5-drobota-bd94aa0a3e49.herokuapp.com/'
+// axios.defaults.baseURL = 'https://pp5-drobota-bd94aa0a3e49.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
